ce7803ad6a9504eceae0383dac523d9a7186a57c
Hamoly <Hamoly@hotmail.com>
2020-04-03 04:50:46 +0200

    ToDo App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
+
+
+const TodoForm = ({addTodo}) => {
+  let input;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={(e) => {
+        e.preventDefault();
+        addTodo(input.value);
+        input.value = '';
+      }}>
+      <input className="form-control col-md-12" ref={node => {
+        input = node;
+      }} 
+      placeholder="Type to add new task then press enter ..."
+      aria-label="New task input"
+      autoFocus/>
+      <br />
+    </form>
   );
+};
+
+const Todo = ({todo}) => {
+  // Each Todo
+  return (<li className="list-group-item">{todo.text}</li>);
 }
 
-export default App;
+const TodoList = ({todos}) => {
+  // Map through the todos
+  const todoNode = todos.map((todo) => {
+    return (<Todo todo={todo} key={todo.id}/>)
+  });
+  return (<ul className="list-group" style={{marginTop:'30px'}}>{todoNode}</ul>);
+}
+
+window.id = 0;
+class TodoApp extends React.Component{
+  constructor(props){
+    // Pass props to parent class
+    super(props);
+    // Set initial state
+    this.state = {
+      data: []
+    }
+  }
+  addTodo(val){
+    const todo = {text: val, id: window.id++}
+    this.state.data.push(todo);
+    this.setState({data: this.state.data});
+  }
+
+
+  render(){
+    return (
+      <div className="container">
+        <TodoForm addTodo={this.addTodo.bind(this)}/>
+        <TodoList 
+          todos={this.state.data} 
+        />
+      </div>
+    );
+  }
+}
+export default TodoApp;
